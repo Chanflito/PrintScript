0942e90668d5ba6a6167cec64904692a4df58455
Luciano Gianfelice <luciano.gianfelice@ing.austral.edu.ar>
2024-03-09 15:03:02 -0300

    feat: add common method on position.

--- src/main/kotlin/common/token/Position.kt
+++ src/main/kotlin/common/token/Position.kt
@@ -3,3 +3,17 @@ package common.token
 data class Position(val row:Int,
                     val column:Int){
 }
+
+fun calculatePosition(code: String, index: Int): Position {
+    var row = 1
+    var column = 1
+    for (i in 0 until index) {
+        if (code[i] == '\n') {
+            row++
+            column = 1
+        } else {
+            column++
+        }
+    }
+    return Position(row, column)
+}
