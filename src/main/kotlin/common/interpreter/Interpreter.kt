5e1c1f3d78cd95cb1275a7c75623c06696ef40e6
Luciano Gianfelice <luciano.gianfelice@ing.austral.edu.ar>
2024-03-14 01:47:27 -0300

    feat: add dummy implementation of interpreter

--- src/main/kotlin/common/interpreter/Interpreter.kt
+++ src/main/kotlin/common/interpreter/Interpreter.kt
@@ -1,42 +1,46 @@
 package common.interpreter
-import common.ast
+import common.ast.ASTNode
+import common.ast.NodeType
+import common.token.TokenType
 
 class Interpreter(val astList: List<ASTNode>) {
     fun interpret() {
         val printNodes = astList.filter { it.nodeType == NodeType.PRINT_NODE }
-        val assignmentNodes = astList.filter { it.nodeType == NodeType.ASSIGNMENT }
-        val variables = buildMap(emptyMap(), assignmentNodes)
+        val assignmentNodes = astList.filter { it.nodeType == NodeType.ASSIGNMENT_NODE }
+        val variables = buildMap(emptyMap(), assignmentNodes.toMutableList())
         printNodes.forEach {printLine(it, variables)}
     }
 
-    private fun buildMap(variables: Map<String, String>, nodes: List<ASTNode>): Map<String, String> {
+    private fun buildMap(variables: Map<String?, String?>, nodes: MutableList<ASTNode>): Map<String?, String?> {
         if(nodes.isEmpty()) return variables
-        val node = nodes.removeAt(0)
+        val node = nodes.removeAt(0);
         val name = findName(node)
         val value = findValue(node)
         return buildMap(variables.plus(Pair(name, value)), nodes)
     }
 
-    private fun findValue(node: ASTNode): String {
-        return when (node.token?.type) {
-            TokenType.VALUE_STRING -> node.token.value
-            TokenType.VALUE_NUMBER -> node.token.value.toString()
+    private fun findValue(node: ASTNode): String? {
+        return when (node.token?.tokenType) {
+            TokenType.VALUE_STRING -> node.token!!.value
+            TokenType.VALUE_NUMBER -> node.token!!.value
             else -> {
-                node.children.fold("") {acc, child -> acc + findValue(child)}
+                node.children?.fold("") { acc, child -> acc + findValue(child)}
             }
         }
     }
 
-    private fun findName(node: ASTNode):String {
-        return when (node.token?.type) {
-            TokenType.IDENTIFIER -> node.token.value
+    private fun findName(node: ASTNode): String? {
+        return when (node.token?.tokenType) {
+            TokenType.IDENTIFIER -> node.token!!.value
             else -> {
-                node.children.fold("") {acc, child -> acc + findName(child)}
+                node.children?.fold("") { acc, child -> acc + findName(child)}
             }
         }
     }
-
-    private fun printLine(node: ASTNode, variables: Map<String, String>) {
-
+    //TODO: Refactor this to use a visitor pattern
+    private fun printLine(node: ASTNode, variables: Map<String?, String?>) {
+        val value = findValue(node)
+        val valueToPrint = if(value?.startsWith("\"") == true) value.substring(1, value.length - 1) else variables[value]
+        println(valueToPrint)
     }
 }
