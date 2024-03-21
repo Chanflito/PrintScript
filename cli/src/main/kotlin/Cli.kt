29e1b6491f503b79110a156c3db29139ef57d18e
Emiliano <emiliano.lopez@ing.austral.edu.ar>
2024-03-21 06:06:29 -0300

    add first cli version (just printing input)

---
+++ cli/src/main/kotlin/Cli.kt
@@ -0,0 +1,31 @@
+fun main(){
+    welcome()
+    do{
+        print("> ")
+        when(val command = readln()){
+            "exit" -> break
+            "help" -> help()
+            else -> println(command)
+        }
+    } while (true)
+}
+
+private fun welcome() {
+    println(
+        "██████╗ ██████╗ ██╗███╗   ██╗████████╗███████╗ ██████╗██████╗ ██╗██████╗ ████████╗\n" +
+                "██╔══██╗██╔══██╗██║████╗  ██║╚══██╔══╝██╔════╝██╔════╝██╔══██╗██║██╔══██╗╚══██╔══╝\n" +
+                "██████╔╝██████╔╝██║██╔██╗ ██║   ██║   ███████╗██║     ██████╔╝██║██████╔╝   ██║   \n" +
+                "██╔═══╝ ██╔══██╗██║██║╚██╗██║   ██║   ╚════██║██║     ██╔══██╗██║██╔═══╝    ██║   \n" +
+                "██║     ██║  ██║██║██║ ╚████║   ██║   ███████║╚██████╗██║  ██║██║██║        ██║   \n" +
+                "╚═╝     ╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝   ╚═╝   ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝        ╚═╝   \n"
+    )
+
+    println("Welcome to printScript! Type 'help' to see more info")
+}
+
+private fun help() {
+    println("\nPrintScript Help:")
+    println("PrintScript is a TypeScript-based programming language.")
+    println("It has only one function:")
+    println("  - println(): Prints anything you pass as a parameter.")
+}
