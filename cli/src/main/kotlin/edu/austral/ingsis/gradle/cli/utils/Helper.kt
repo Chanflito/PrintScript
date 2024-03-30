edca94e06f9e2b109b542b91ab815f95956cb6b8
Luciano Gianfelice <luciano.gianfelice@ing.austral.edu.ar>
2024-03-29 22:26:44 -0300

    refactor: delete unused method

--- cli/src/main/kotlin/edu/austral/ingsis/gradle/cli/utils/Helper.kt
+++ cli/src/main/kotlin/edu/austral/ingsis/gradle/cli/utils/Helper.kt
@@ -1,15 +1,5 @@
 package edu.austral.ingsis.gradle.cli.utils
 
-fun clearConsole() {
-    // Clearing the console based on the operating system
-    val os = System.getProperty("os.name")
-    when {
-        os.contains("Windows") -> Runtime.getRuntime().exec("cmd /c cls")
-        os.contains("Linux") || os.contains("Mac") -> Runtime.getRuntime().exec("clear")
-        else -> println("Unsupported operating system: $os")
-    }
-}
-
 fun delay(milliseconds: Long) {
     try {
         Thread.sleep(milliseconds)
