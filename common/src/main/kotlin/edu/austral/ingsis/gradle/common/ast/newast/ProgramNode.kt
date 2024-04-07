7c25ef781eca02f6abeadc879735d9755a02751c
valendc15 <valentino.dicapua@ing.austral.edu.ar>
2024-04-07 20:08:15 -0300

    feat: fixed previous errors in ProgramNode

--- common/src/main/kotlin/edu/austral/ingsis/gradle/common/ast/newast/ProgramNode.kt
+++ common/src/main/kotlin/edu/austral/ingsis/gradle/common/ast/newast/ProgramNode.kt
@@ -2,8 +2,4 @@ package edu.austral.ingsis.gradle.common.ast.newast
 
 import edu.austral.ingsis.gradle.common.token.TokenPosition
 
-data class ProgramNode(override val tokenPosition: TokenPosition, val children: List<Statement>) : AST {
-    fun getChildren(): List<Statement> {
-        return children
-    }
-}
+data class ProgramNode(override val tokenPosition: TokenPosition, val children: List<Statement>) : AST
