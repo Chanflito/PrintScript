6c21d841a379fb0b751c2f02feb12e2b7183eecb
nicolas <nicolas.mangini@ing.austral.edu.ar>
2024-04-17 23:40:47 -0300

    refactor: move ast to Input file

--- formatter/src/test/kotlin/edu/austral/ingsis/gradle/formatter/FormatterTest.kt
+++ formatter/src/test/kotlin/edu/austral/ingsis/gradle/formatter/FormatterTest.kt
@@ -1,22 +1,5 @@
 package edu.austral.ingsis.gradle.formatter
 
-import edu.austral.ingsis.gradle.common.ast.newast.BlockNode
-import edu.austral.ingsis.gradle.common.ast.newast.DeclarationAssignation
-import edu.austral.ingsis.gradle.common.ast.newast.IdentifierNode
-import edu.austral.ingsis.gradle.common.ast.newast.IfElseStatement
-import edu.austral.ingsis.gradle.common.ast.newast.IfStatement
-import edu.austral.ingsis.gradle.common.ast.newast.LetKeywordNode
-import edu.austral.ingsis.gradle.common.ast.newast.NumberLiteralNode
-import edu.austral.ingsis.gradle.common.ast.newast.PrintLnNode
-import edu.austral.ingsis.gradle.common.ast.newast.ProgramNode
-import edu.austral.ingsis.gradle.common.ast.newast.ReAssignationNode
-import edu.austral.ingsis.gradle.common.ast.newast.ReadInputNode
-import edu.austral.ingsis.gradle.common.ast.newast.StringLiteral
-import edu.austral.ingsis.gradle.common.ast.newast.StringNodeType
-import edu.austral.ingsis.gradle.common.ast.newast.SubtractNode
-import edu.austral.ingsis.gradle.common.ast.newast.SumNode
-import edu.austral.ingsis.gradle.common.token.Position
-import edu.austral.ingsis.gradle.common.token.TokenPosition
 import edu.austral.ingsis.gradle.formatter.rule.ComposeRule
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
@@ -27,20 +10,7 @@ class FormatterTest {
         val formatter = createDefaultFormatter()
         val defaultRules = ComposeRule(createDefaultRules("src/test/resources/config_001.json"))
         val ifBlockRules = ComposeRule(createIfBlockRules("src/test/resources/config_001.json"))
-        val ast =
-            ProgramNode(
-                TokenPosition(Position(0, 0), Position(0, 0)),
-                listOf(
-                    DeclarationAssignation(
-                        LetKeywordNode(TokenPosition(Position(0, 0), Position(0, 0))),
-                        TokenPosition(Position(0, 0), Position(0, 0)),
-                        StringNodeType,
-                        IdentifierNode("aBlue", TokenPosition(Position(0, 0), Position(0, 0))),
-                        StringLiteral("blue", TokenPosition(Position(0, 0), Position(0, 0))),
-                    ),
-                ),
-            )
-
+        val ast = input001
         val formatted = formatter.format(ast, defaultRules, ifBlockRules)
         assertEquals(expected = "let aBlue : String = \"blue\";", actual = formatted)
     }
@@ -50,44 +20,7 @@ class FormatterTest {
         val formatter = createDefaultFormatter()
         val defaultRules = ComposeRule(createDefaultRules("src/test/resources/config_001.json"))
         val ifBlockRules = ComposeRule(createIfBlockRules("src/test/resources/config_001.json"))
-        val ast =
-            ProgramNode(
-                TokenPosition(Position(0, 0), Position(0, 0)),
-                listOf(
-                    IfStatement(
-                        TokenPosition(Position(0, 0), Position(0, 0)),
-                        condition = IdentifierNode("a", TokenPosition(Position(0, 0), Position(0, 0))),
-                        ifBlock =
-                            BlockNode(
-                                TokenPosition(Position(0, 0), Position(0, 0)),
-                                listOf(
-                                    PrintLnNode(
-                                        TokenPosition(Position(0, 0), Position(0, 0)),
-                                        SumNode(
-                                            TokenPosition(Position(0, 0), Position(0, 0)),
-                                            IdentifierNode(
-                                                "a",
-                                                TokenPosition(Position(0, 0), Position(0, 0)),
-                                            ),
-                                            IdentifierNode(
-                                                "b",
-                                                TokenPosition(Position(0, 0), Position(0, 0)),
-                                            ),
-                                        ),
-                                    ),
-                                    DeclarationAssignation(
-                                        LetKeywordNode(TokenPosition(Position(0, 0), Position(0, 0))),
-                                        TokenPosition(Position(0, 0), Position(0, 0)),
-                                        StringNodeType,
-                                        IdentifierNode("aBlue", TokenPosition(Position(0, 0), Position(0, 0))),
-                                        StringLiteral("blue", TokenPosition(Position(0, 0), Position(0, 0))),
-                                    ),
-                                ),
-                            ),
-                    ),
-                ),
-            )
-
+        val ast = input002
         val formatted = formatter.format(ast, defaultRules, ifBlockRules)
         assertEquals(expected = "if (a) {\n   println(a + b);\n   let aBlue : String = \"blue\";\n}", actual = formatted)
     }
@@ -97,57 +30,7 @@ class FormatterTest {
         val formatter = createDefaultFormatter()
         val defaultRules = ComposeRule(createDefaultRules("src/test/resources/config_001.json"))
         val ifBlockRules = ComposeRule(createIfBlockRules("src/test/resources/config_001.json"))
-        val ast =
-            ProgramNode(
-                TokenPosition(Position(0, 0), Position(0, 0)),
-                listOf(
-                    IfElseStatement(
-                        TokenPosition(Position(0, 0), Position(0, 0)),
-                        condition = IdentifierNode("a", TokenPosition(Position(0, 0), Position(0, 0))),
-                        ifBlock =
-                            BlockNode(
-                                TokenPosition(Position(0, 0), Position(0, 0)),
-                                listOf(
-                                    PrintLnNode(
-                                        TokenPosition(Position(0, 0), Position(0, 0)),
-                                        SumNode(
-                                            TokenPosition(Position(0, 0), Position(0, 0)),
-                                            IdentifierNode(
-                                                "a",
-                                                TokenPosition(Position(0, 0), Position(0, 0)),
-                                            ),
-                                            IdentifierNode(
-                                                "b",
-                                                TokenPosition(Position(0, 0), Position(0, 0)),
-                                            ),
-                                        ),
-                                    ),
-                                ),
-                            ),
-                        elseBlock =
-                            BlockNode(
-                                TokenPosition(Position(0, 0), Position(0, 0)),
-                                listOf(
-                                    PrintLnNode(
-                                        TokenPosition(Position(0, 0), Position(0, 0)),
-                                        SubtractNode(
-                                            TokenPosition(Position(0, 0), Position(0, 0)),
-                                            IdentifierNode(
-                                                "a",
-                                                TokenPosition(Position(0, 0), Position(0, 0)),
-                                            ),
-                                            IdentifierNode(
-                                                "b",
-                                                TokenPosition(Position(0, 0), Position(0, 0)),
-                                            ),
-                                        ),
-                                    ),
-                                ),
-                            ),
-                    ),
-                ),
-            )
-
+        val ast = input003
         val formatted = formatter.format(ast, defaultRules, ifBlockRules)
         assertEquals(expected = "if (a) {\n   println(a + b);\n}else {\n   println(a - b);\n}", actual = formatted)
     }
@@ -157,21 +40,7 @@ class FormatterTest {
         val formatter = createDefaultFormatter()
         val defaultRules = ComposeRule(createDefaultRules("src/test/resources/config_001.json"))
         val ifBlockRules = ComposeRule(createIfBlockRules("src/test/resources/config_001.json"))
-        val ast =
-            ProgramNode(
-                TokenPosition(Position(0, 0), Position(0, 0)),
-                listOf(
-                    ReAssignationNode(
-                        TokenPosition(Position(0, 0), Position(0, 0)),
-                        NumberLiteralNode(
-                            1,
-                            TokenPosition(Position(0, 0), Position(0, 0)),
-                        ),
-                        IdentifierNode("a", TokenPosition(Position(0, 0), Position(0, 0))),
-                    ),
-                ),
-            )
-
+        val ast = input004
         val formatted = formatter.format(ast, defaultRules, ifBlockRules)
         assertEquals(expected = "a = 1;", actual = formatted)
     }
@@ -181,35 +50,7 @@ class FormatterTest {
         val formatter = createDefaultFormatter()
         val defaultRules = ComposeRule(createDefaultRules("src/test/resources/config_001.json"))
         val ifBlockRules = ComposeRule(createIfBlockRules("src/test/resources/config_001.json"))
-        val ast =
-            ProgramNode(
-                TokenPosition(Position(0, 0), Position(0, 0)),
-                listOf(
-                    DeclarationAssignation(
-                        LetKeywordNode(TokenPosition(Position(0, 0), Position(0, 0))),
-                        TokenPosition(Position(0, 0), Position(0, 0)),
-                        StringNodeType,
-                        IdentifierNode("aBlue", TokenPosition(Position(0, 0), Position(0, 0))),
-                        StringLiteral("blue", TokenPosition(Position(0, 0), Position(0, 0))),
-                    ),
-                    ReAssignationNode(
-                        TokenPosition(Position(0, 0), Position(0, 0)),
-                        NumberLiteralNode(
-                            1,
-                            TokenPosition(Position(0, 0), Position(0, 0)),
-                        ),
-                        IdentifierNode("a", TokenPosition(Position(0, 0), Position(0, 0))),
-                    ),
-                    DeclarationAssignation(
-                        LetKeywordNode(TokenPosition(Position(0, 0), Position(0, 0))),
-                        TokenPosition(Position(0, 0), Position(0, 0)),
-                        StringNodeType,
-                        IdentifierNode("aBlue", TokenPosition(Position(0, 0), Position(0, 0))),
-                        StringLiteral("blue", TokenPosition(Position(0, 0), Position(0, 0))),
-                    ),
-                ),
-            )
-
+        val ast = input005
         val formatted = formatter.format(ast, defaultRules, ifBlockRules)
         assertEquals(expected = "let aBlue : String = \"blue\";\na = 1;\nlet aBlue : String = \"blue\";", actual = formatted)
     }
@@ -219,34 +60,7 @@ class FormatterTest {
         val formatter = createDefaultFormatter()
         val defaultRules = ComposeRule(createDefaultRules("src/test/resources/config_001.json"))
         val ifBlockRules = ComposeRule(createIfBlockRules("src/test/resources/config_001.json"))
-        val ast =
-            ProgramNode(
-                TokenPosition(Position(0, 0), Position(0, 0)),
-                listOf(
-                    PrintLnNode(
-                        TokenPosition(Position(0, 0), Position(0, 0)),
-                        SumNode(
-                            TokenPosition(Position(0, 0), Position(0, 0)),
-                            IdentifierNode(
-                                "a",
-                                TokenPosition(Position(0, 0), Position(0, 0)),
-                            ),
-                            IdentifierNode(
-                                "b",
-                                TokenPosition(Position(0, 0), Position(0, 0)),
-                            ),
-                        ),
-                    ),
-                    ReadInputNode(
-                        TokenPosition(Position(0, 0), Position(0, 0)),
-                        IdentifierNode(
-                            "Introduzca un valor",
-                            TokenPosition(Position(0, 0), Position(0, 0)),
-                        ),
-                    ),
-                ),
-            )
-
+        val ast = input006
         val formatted = formatter.format(ast, defaultRules, ifBlockRules)
         assertEquals(expected = "println(a + b);\nreadInput(Introduzca un valor);", actual = formatted)
     }
