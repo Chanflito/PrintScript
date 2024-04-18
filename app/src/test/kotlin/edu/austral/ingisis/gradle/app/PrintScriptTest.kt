ad2af04d2ed27a7f817d8ea0940316cbd1427c27
Luciano Gianfelice <luciano.gianfelice@ing.austral.edu.ar>
2024-04-18 00:00:34 -0300

    refactor: comment old tests

--- app/src/test/kotlin/edu/austral/ingisis/gradle/app/PrintScriptTest.kt
+++ app/src/test/kotlin/edu/austral/ingisis/gradle/app/PrintScriptTest.kt
@@ -1,70 +1,63 @@
 package edu.austral.ingisis.gradle.app
 
-import edu.austral.ingsis.gradle.lexer.director.LexerDirector
-import edu.austral.ingsis.gradle.parser.InputContext
-import edu.austral.ingsis.gradle.parser.util.createComposeParser
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
-import kotlin.test.Ignore
-
 class PrintScriptTest {
-    @Test
-    @Ignore
-    fun test_HelloWorldPrint() {
-        val code = "let a : string = \"Hello, World!\"; println(a);" // Print "Hello, World!"
-        val tokens = LexerDirector().createComposeLexer("1.0").splitIntoTokens(code)
-        val ast = createComposeParser().parse(InputContext(tokens, 0)).first
-        // val result = Interpreter().interpret(ast)
-        assertEquals("Hello, World!", TODO())
-    }
-
-    @Test
-    @Ignore
-    fun test_Addition() {
-        val code = "let a : number = 1; let b : number = 2; println(a + b);" // Print 3
-        val tokens = LexerDirector().createComposeLexer("1.0").splitIntoTokens(code)
-        val ast = createComposeParser().parse(InputContext(tokens, 0)).first
-        // val result = Interpreter().interpret(ast)
-        assertEquals(3.0, TODO())
-    }
-
-    @Test
-    @Ignore
-    fun test_Subtraction() {
-        val code = "let a : number = 1; let b : number = 2; println(a - b);" // Print -1
-        val tokens = LexerDirector().createComposeLexer("1.0").splitIntoTokens(code)
-        val ast = createComposeParser().parse(InputContext(tokens, 0)).first
-        // val result = Interpreter().interpret(ast)
-        assertEquals(-1.0, TODO())
-    }
-
-    @Test
-    @Ignore
-    fun test_Multiplication() {
-        val code = "let a : number = 2; let b : number = 3; println(a * b);" // Print 6
-        val tokens = LexerDirector().createComposeLexer("1.0").splitIntoTokens(code)
-        val ast = createComposeParser().parse(InputContext(tokens, 0)).first
-        // val result = Interpreter().interpret(ast) implement when new interpreter is ready
-        assertEquals(6.0, TODO())
-    }
-
-    @Test
-    @Ignore
-    fun test_Division() {
-        val code = "let a : number = 6; let b : number = 3; println(a / b);" // Print 2
-        val tokens = LexerDirector().createComposeLexer("1.0").splitIntoTokens(code)
-        val ast = createComposeParser().parse(InputContext(tokens, 0)).first
-        // val result = Interpreter().interpret(ast)
-        assertEquals(2.0, TODO())
-    }
-
-    @Test
-    @Ignore
-    fun test_StringAddition() {
-        val code = "let a : string = \"Hello, \"; let b : string = \"World!\"; println(a + b);" // Print "Hello, World!"
-        val tokens = LexerDirector().createComposeLexer("1.0").splitIntoTokens(code)
-        val ast = createComposeParser().parse(InputContext(tokens, 0)).first
-        // val result = Interpreter().interpret(ast)
-        assertEquals("Hello, World!", TODO())
-    }
+//    @Test
+//    @Ignore
+//    fun test_HelloWorldPrint() {
+//        val code = "let a : string = \"Hello, World!\"; println(a);" // Print "Hello, World!"
+//        val tokens = LexerDirector().createComposeLexer("1.0").splitIntoTokens(code)
+//        val ast = createComposeParser().parse(InputContext(tokens, 0)).first
+//        // val result = Interpreter().interpret(ast)
+//        assertEquals("Hello, World!", TODO())
+//    }
+//
+//    @Test
+//    @Ignore
+//    fun test_Addition() {
+//        val code = "let a : number = 1; let b : number = 2; println(a + b);" // Print 3
+//        val tokens = LexerDirector().createComposeLexer("1.0").splitIntoTokens(code)
+//        val ast = createComposeParser().parse(InputContext(tokens, 0)).first
+//        // val result = Interpreter().interpret(ast)
+//        assertEquals(3.0, TODO())
+//    }
+//
+//    @Test
+//    @Ignore
+//    fun test_Subtraction() {
+//        val code = "let a : number = 1; let b : number = 2; println(a - b);" // Print -1
+//        val tokens = LexerDirector().createComposeLexer("1.0").splitIntoTokens(code)
+//        val ast = createComposeParser().parse(InputContext(tokens, 0)).first
+//        // val result = Interpreter().interpret(ast)
+//        assertEquals(-1.0, TODO())
+//    }
+//
+//    @Test
+//    @Ignore
+//    fun test_Multiplication() {
+//        val code = "let a : number = 2; let b : number = 3; println(a * b);" // Print 6
+//        val tokens = LexerDirector().createComposeLexer("1.0").splitIntoTokens(code)
+//        val ast = createComposeParser().parse(InputContext(tokens, 0)).first
+//        // val result = Interpreter().interpret(ast) implement when new interpreter is ready
+//        assertEquals(6.0, TODO())
+//    }
+//
+//    @Test
+//    @Ignore
+//    fun test_Division() {
+//        val code = "let a : number = 6; let b : number = 3; println(a / b);" // Print 2
+//        val tokens = LexerDirector().createComposeLexer("1.0").splitIntoTokens(code)
+//        val ast = createComposeParser().parse(InputContext(tokens, 0)).first
+//        // val result = Interpreter().interpret(ast)
+//        assertEquals(2.0, TODO())
+//    }
+//
+//    @Test
+//    @Ignore
+//    fun test_StringAddition() {
+//        val code = "let a : string = \"Hello, \"; let b : string = \"World!\"; println(a + b);" // Print "Hello, World!"
+//        val tokens = LexerDirector().createComposeLexer("1.0").splitIntoTokens(code)
+//        val ast = createComposeParser().parse(InputContext(tokens, 0)).first
+//        // val result = Interpreter().interpret(ast)
+//        assertEquals("Hello, World!", TODO())
+//    }
 }
