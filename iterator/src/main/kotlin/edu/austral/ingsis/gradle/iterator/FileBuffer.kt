040f8f6665e298af79ec60c5b69bf113590f289d
Luciano Gianfelice <luciano.gianfelice@ing.austral.edu.ar>
2024-04-20 21:53:15 -0300

    feat: change lines

--- iterator/src/main/kotlin/edu/austral/ingsis/gradle/iterator/FileBuffer.kt
+++ iterator/src/main/kotlin/edu/austral/ingsis/gradle/iterator/FileBuffer.kt
@@ -8,11 +8,11 @@ class FileBuffer(private val file: InputStream) {
     private val bufferReader = BufferedReader(InputStreamReader(file))
 
     companion object {
-        const val MAX_LINES = 10
+        const val READ_AHEAD_LIMIT = 8000
     }
 
     fun canRead(): Boolean {
-        bufferReader.mark(MAX_LINES)
+        bufferReader.mark(READ_AHEAD_LIMIT)
         val canRead = bufferReader.readLine() != null
         bufferReader.reset()
         return canRead
