d8dd9c906c26bba611fee71e223c907947f80b1a
n-mangini <nicolas.mangini@ing.austral.edu.ar>
2024-04-25 21:21:03 -0300

    refactor: enhance readability

--- formatter/src/main/kotlin/edu/austral/ingsis/gradle/formatter/rule/SpaceBeforeColonRule.kt
+++ formatter/src/main/kotlin/edu/austral/ingsis/gradle/formatter/rule/SpaceBeforeColonRule.kt
@@ -1,10 +1,10 @@
 package edu.austral.ingsis.gradle.formatter.rule
 
-import edu.austral.ingsis.gradle.formatter.util.spaceBeforeColonRegex
+import edu.austral.ingsis.gradle.formatter.util.contentBeforeColonRegex
 
 class SpaceBeforeColonRule : Rule {
     override fun applyRule(code: String): String {
-        val replaced = code.replace(spaceBeforeColonRegex, "$1 :")
+        val replaced = code.replace(contentBeforeColonRegex, "$1 :")
         return replaced
     }
 }
--- formatter/src/main/kotlin/edu/austral/ingsis/gradle/formatter/util/RegexPatterns.kt
+++ formatter/src/main/kotlin/edu/austral/ingsis/gradle/formatter/util/RegexPatterns.kt
@@ -1,6 +1,6 @@
 package edu.austral.ingsis.gradle.formatter.util
 
-val spaceBeforeColonRegex = Regex("(\\w+)(\\s*):")
-val spaceAfterColonRegex = Regex(":(\\s*)(\\w+)")
-val spaceAroundEqualRegex = Regex("(\\w+)(\\s*)(=)(\\s*)(\"[^\"]*\"|\\w+)")
-val newlineBeforePrintlnRegex = Regex("(?<=\\W)println")
+val contentBeforeColonRegex = Regex("(\\w+)(\\s*):")
+val contentAfterColonRegex = Regex(":(\\s*)(\\w+)")
+val contentAroundEqualRegex = Regex("(\\w+)(\\s*)(=)(\\s*)(\\w+)")
+val lineBeforePrintlnRegex = Regex("(?<=\\W)println")
